815091ef61f82fe3b279b3e4176f18da59c91664
finne132 <finne132@umn.edu>
2018-03-22 15:09:18 -0500

    totally forgot to commit a SHIT load of work...

--- main.js
+++ main.js
@@ -1,45 +1,63 @@
 "use strict";
+$( document ).ready(function() {
+    console.log( "ready!" );
 
 var shows = ["Arrested Development", "Parks and Recreation", "Peep Show", "Aqua Teen Hunger Force"];
 
-      // Function for displaying movie data
-      function renderButtons() {
+function displayShowGIFS() {
+    $("#gifs-display").empty();
+    var show = $(this).attr("data-name"); //get text value of button pressed
+    show=show.replace(/ /g,"_"); //replace spaces in the name with underscores (the API needs this)
+    var limit = 10; //how many gifs to display 
+    var apiKey = "dc6zaTOxFJmzC"; // my API key
+    var queryURL = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${show}&limit=${limit}&lang=en`; // query URL to use
 
-        // Deleting the movie buttons prior to adding new movie buttons
+    console.log(`using URL ${queryURL}`)
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log("completed AJAX request");
+        for (var i = 0; i <=response.data.length;i++){
+
+       
+        var src = JSON.stringify(response.data[i].images.fixed_height_downsampled.url);
+        console.log(`src is ${src}`)
+      $("#gifs-display").append(`<img src=${src}>`);
+      console.log(`<img src=${src}>`);
+        };
+      renderButtons();
+
+       
+    });
+  }
+
+      function renderButtons() {
+        // Deleting the show buttons prior to adding new movie buttons
         // (this is necessary otherwise we will have repeat buttons)
         $("#gifs-view").empty();
-
-        // Looping through the array of movies
+        // Looping through the array of shows to make a button for each
         for (var i = 0; i < shows.length; i++) {
-
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
           var a = $("<button>");
-          // Adding a class
           a.addClass("show");
-          // Adding a data-attribute with a value of the movie at index i
-          a.attr("data-name", movies[i]);
-          // Providing the button's text with a value of the movie at index i
-          a.text(movies[i]);
-          // Adding the button to the HTML
-          $("#gifs-view").append(a);
+          a.attr("data-name", shows[i]);
+          a.text(shows[i]);
+          $("#gifs-view").append(a);  // add the button to the HTML with jQuery
         }
       }
 
-      // This function handles events where one button is clicked
+      // when the add button is clicked
       $("#add-show").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
+        // prevents the form from trying to submit itself and reload the page
         event.preventDefault();
-
-        // This line will grab the text from the input box
-        var shows = $("#show-input").val().trim();
-        // The movie from the textbox is then added to our array
-        shows.push(show);
-
-        // calling renderButtons which handles the processing of our movie array
-        renderButtons();
+        var show = $("#show-input").val().trim(); // get text from the input box
+        shows.push(show); // add it to the show array
+        renderButtons(); //re-render the buttons
       });
-
-      // Calling the renderButtons function at least once to display the initial list of movies
-      renderButtons();
+    
+    // handles what happens when someone clicks a show title button (runs the ajax function)
+    $(document).on("click", ".show", displayShowGIFS);
+    
+      renderButtons(); //call render buttons to have some default buttons displayed when the page loads
+    });
